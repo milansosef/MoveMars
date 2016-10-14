23365ad19d2f6bd7e9f1c1b31dd6c5208707ab01
Milan Sosef <Milan Sosef>
2016-10-14 12:35:40 +0200

    Beginnetje aan de website

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Milan
+ * Date: 12-10-2016
+ * Time: 15:01
+ */
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mars Move</title>
+    <link rel="stylesheet" type="text/css" href="css/MoveMars.css">
+</head>
+<body>
+<ul class="navigatiebalk">
+      <li class="left"><a href="index.php">Home</a></li>
+      <li class="left"><a href="store.php">Store</a></li>
+      <li class="left"><a href="partners.php">Partners</a></li>
+      <li class="left"><a href="contact.php">Contact</a></li>
+      <li class="middle"></li>
+      <li class="right"><a href="login.php">Login</a></li>
+      <li class="right"><a href="register.php">Register</a></li>
+</ul>';
+
+?>
+
+.
+.
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include_once "header.php"?>
+<div style="background-color: #ff4507; width: 100%; height: 100px;">
+    <h1>Title</h1>
+</div>
+<div style="background-color: #fffebb; width: 100%; height: 100px;">
+</div>
+
+
+<?php include_once "footer.php"?>
