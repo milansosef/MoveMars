d283ccda8a4bac3166e4ef107f9b60478aa9345a
HarisSpahija <harisspahija@gmail.com>
2016-10-20 15:06:37 +0200

    Add files via upload

---
+++ MoveMars2.0/Core/database/connect.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HAL9000
+ * Date: 20-10-2016
+ * Time: 14:34
+ */
+mysql_connect('localhost', 'root', '');
+mysql_select_db(lr);
+?>
---
+++ MoveMars2.0/Core/functions/init.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HAL9000
+ * Date: 20-10-2016
+ * Time: 14:35
+ */
+session_start();
+
+require 'datanse/connect.php';
+
+?>
