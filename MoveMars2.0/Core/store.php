afb0b87c73568ec867c5cf631d0418d59a0a1f8d
milansosef <msosef@yahoo.com>
2016-10-26 12:26:55 +0200

    Update store.php

--- MoveMars2.0/Core/store.php
+++ MoveMars2.0/Core/store.php
@@ -1,6 +1,6 @@
 <?php include_once "includes/header.php"?>
 
-
+<p>Currently out of stock. For more updates, check our Facebook, Twitter and Instagram page.</p>
 
 
 <?php include_once "includes/footer.php"?>
