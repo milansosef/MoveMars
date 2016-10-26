3a211c1c86919de2cf6192d00fc1a9e12456d149
milansosef <msosef@yahoo.com>
2016-10-26 12:54:52 +0200

    Update partners.php

--- MoveMars2.0/Core/partners.php
+++ MoveMars2.0/Core/partners.php
@@ -1,7 +1,9 @@
 <?php include_once "includes/header.php"?>
 
-<p>Have a look at NASA's site for more information about space travelling</p>
-<a href="http://www.nasa.gov/"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1237px-NASA_logo.svg.png" width="25%" height="25%"> </a>
+<div class="content" align="center">
+    <a href="http://www.nasa.gov/"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1237px-NASA_logo.svg.png" width="25%" height="25%"> </a>
+    <p>Have a look at NASA's site for more information about space travelling</p>
+</div>
 
 
 <?php include_once "includes/footer.php"?>
