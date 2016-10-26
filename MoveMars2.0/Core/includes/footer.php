78d0c5e3b9e971424e032b3301ff81288bb892ce
milansosef <msosef@yahoo.com>
2016-10-26 12:28:21 +0200

    Update footer.php

--- MoveMars2.0/Core/includes/footer.php
+++ MoveMars2.0/Core/includes/footer.php
@@ -6,7 +6,15 @@
  * Time: 15:01
  */
 
-echo '</body>
+
+echo '
+    <div class="footer">
+        <p>Follow us on social media</p>
+            <a href="https://www.facebook.com/MarsMoveInc/"> <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png" class="foto"</a>
+            <a href="https://twitter.com/MarsMoveInc"> <img src="http://icons.iconarchive.com/icons/designbolts/cute-social-media/256/Twitter-icon.png" class="foto"</a>
+            <a href="https://www.instagram.com/marsmoveinc/"> <img src="http://d5vf6134d8ffdnfp1qv4rv3l.wpengine.netdna-cdn.com/wp-content/uploads/Instagram-v051916_200.png" class="foto"</a>
+    </div>
+</body>
 </html>'
 
 ?>
