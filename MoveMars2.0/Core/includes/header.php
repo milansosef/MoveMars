d4e40b9a4db7cbe6e80daa9b8de7d7435474f84e
HarisSpahija <harisspahija@gmail.com>
2016-10-26 12:25:15 +0200

    header 1.5
    Added account functions and session with login/logout!

--- MoveMars2.0/Core/includes/header.php
+++ MoveMars2.0/Core/includes/header.php
@@ -4,6 +4,7 @@
  * User: Milan
  * Date: 12-10-2016
  * Time: 15:01
+ * Edit: Haris, 26-10-2016 12:
  */
 echo '
 <!DOCTYPE html>
@@ -11,7 +12,7 @@ echo '
 <head>
     <meta charset="UTF-8">
     <title>Mars Move</title>
-    <link rel="stylesheet" type="text/css" href="../css/MoveMars.css">
+    <link rel="stylesheet" type="text/css" href="MoveMars.css">
 </head>
 <body>
 <ul class="navigatiebalk">
@@ -20,10 +21,17 @@ echo '
       <li class="left"><a href="partners.php">Partners</a></li>
       <li class="left"><a href="contact.php">Contact</a></li>
       <li class="middle"></li>
-      <li class="right"><a href="login.php">Login</a></li>
-      <li class="right"><a href="register.php">Register</a></li>
-</ul class="navigatiebalk">
+       ';
+session_start();
+if(isset($_SESSION['username'])){
 
-'
+    echo '<li class ="right"><a href="myaccount.php">'.$_SESSION['username'].'</a></li>
+                   <li class ="right"><a href="logout.php">Logout</a></li>';
+
+}else{
+    echo '<li class="right"><a href="login.php">Login</a></li>';
+}
+echo '<li class="right"><a href="register.php">Register</a></li>
+              </ul class="navigatiebalk">';
 ?>
 
