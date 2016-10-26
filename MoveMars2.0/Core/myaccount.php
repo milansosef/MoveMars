dd7cd14d6e8e278596b29f0e248fef8971f28fda
HarisSpahija <harisspahija@gmail.com>
2016-10-26 12:22:59 +0200

    myaccount 1.0
    My account version implemented for account usage!!! Still in BETA

---
+++ MoveMars2.0/Core/myaccount.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joey
+ * Date: 26-10-2016
+ * Time: 12:00
+ */
+
+
+
+echo '<p style="font-size: 30px">Unfortunatly your account is unavailable, you will be redirected to the front page...</p>';
+
+echo '
+
+<script>setInterval(function(){
+    window.location.replace("http://localhost/MoveMars2.0/Core/index.php");
+}, 3000)</script>
+
+
+
+
+';
+
+
+?>
